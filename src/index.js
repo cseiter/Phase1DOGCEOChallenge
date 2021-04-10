948298372fc4aa6caf73cd6729227500b9429dd3
chris <cseiter@gmail.com>
2021-04-10 16:01:41 -0400

    first challenge completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c Hello', 'color: firebrick')
+
+//onload functions
+document.addEventListener('DOMContentLoaded', function() {
+  fetchPupperPics();
+});
+
+//use fetch to get random pics of dogs, using response
+//array as inputs for next function
+function fetchPupperPics() {
+	fetch('https://dog.ceo/api/breeds/image/random/4')
+	.then(resp => resp.json())
+//	.then(json => console.log(json))
+	.then(results => {results.message.forEach(pupperPic => appendPic(pupperPic))
+	});
+}
+
+//add individual images from input array to div container
+function appendPic(picURL) {
+	let picDiv = document.getElementById('dog-image-container');
+	let newImgTag = document.createElement('img');
+	newImgTag.src = picURL;
+	picDiv.appendChild(newImgTag);
+}
+
